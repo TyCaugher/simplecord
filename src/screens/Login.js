2eaed0e543a0df1966c054bcc00cb34cb80f0dc7
Tyler Kauffman <tak4506@gmail.com>
2023-06-14 15:00:21 -0500

    Login splash update

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -12,9 +12,11 @@ function Login() {
 
   return (
     <div className='login'>
+        <h1>Simplecord</h1>
         <div className='login__logo'>
             <img src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png" />
         </div>
+        <h3>Discord-inspired chat created by Tyler Kauffman</h3>
         <Button onClick={signIn} >Sign In</Button>
     </div>
   )
